9c4cecd1045502c45142e5d940362a3e0287d573
Wassim <wassimwassim.ka1@gmail.com>
2024-11-11 12:52:57 +0100

    update orders

--- js/script.js
+++ js/script.js
@@ -51,11 +51,78 @@ buttonOrders.onclick = ()=>{
     if(OrderList.style.width == "100%"){
         OrderList.style.width = "0%";
         OrderList.style.border = "none";
+        OrderList.getElementsByClassName("cardOrders")[0].style.transform = "translate(100%)";
     }else{
         OrderList.style.width = "100%";
         OrderList.style.border = "4px solid white";
+        OrderList.getElementsByClassName("cardOrders")[0].style.transform = "translate(0%)";
     }
 }
+updateCounter();
+ordersCalcule();
+getCountOrder();
+removeOrder();
 
 
 })
+
+
+// ========================= Calcule l'orders =============================
+
+
+function ordersCalcule(){
+    let getOrdersSection = document.getElementById("getOrdersSection");
+    let getCards = getOrdersSection.getElementsByClassName("order");
+    let totaleElement = document.getElementById("totaleElement");
+    let totale = 0;
+    let count;
+
+    
+
+    for(i = 0; i < getCards.length; i++){
+        let getPriceOrder = getCards[i].getElementsByClassName("priceOrder")[0];
+        let getPrice = parseInt(getPriceOrder.innerHTML.replace("$",""));
+        let getInputsCount = getCards[i].getElementsByClassName("getInputsCount")[0];
+        count = parseInt(getInputsCount.value);
+        totale += getPrice*count;
+    }
+    totaleElement.innerText = totale + "$";
+}
+
+// ========================= Calcule l'orders =============================
+
+function getCountOrder(){
+    let getInputsCount = document.getElementsByClassName("getInputsCount");
+
+    for(i = 0; i < getInputsCount.length; i++){
+        let allInputsCount = getInputsCount[i];
+        allInputsCount.onchange = ()=>{
+            if(allInputsCount.value < 1){
+                allInputsCount.value = 1;
+            }else{
+                count = allInputsCount.value;
+                ordersCalcule(count);
+            }
+        }
+    }
+    
+}
+
+
+// ========================= update counter =============================
+
+
+function updateCounter(){
+    let cards = document.getElementsByClassName("order").length;
+    let counter = document.getElementById("counter");
+    counter.innerText = cards;
+}
+
+
+// ========================= remove orders =============================
+
+function removeOrder(element){
+    element.parentElement.parentElement.parentElement.remove();
+            ordersCalcule();
+            updateCounter();
+}
