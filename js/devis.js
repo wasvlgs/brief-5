b7aecfed9956a399f11ff733403dc9e493c5d04f
IlyasBh02 <ilyasbahsi9@gmail.com>
2024-11-13 16:47:41 +0100

    add fonction of print

--- js/devis.js
+++ js/devis.js
@@ -3,7 +3,17 @@ document.getElementById('Date').textContent = "Date : " + new Date().toLocaleDat
 document.getElementById('Lieu').textContent = "Lieu : Safi, Maroc: " + new Date().toLocaleTimeString('fr-FR');
 
 
+
 // impr devis
+
 document.getElementById('Imprimer').addEventListener('click', function() {
-    window.print();  // Imprimer la page entière
+    window.print(); 
 });
+
+// function imprElement(Devis){
+//     const contenu = document.getElementById(Devis).innerHTML;
+//     const impr = window.open('', '', 'height=600,width=800');
+//     impr.document.write(`<html><body>${contenu}</body></html>`);
+//     impr.document.close();
+//     impr.print();
+// }
