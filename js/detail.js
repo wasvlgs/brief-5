8e3625b4c32794226b7e428cbadbb4bf110ed6ba
ayuub <ayoubbenomar1230@gmail.com>
2024-11-13 15:17:04 +0100

    update js file

--- js/detail.js
+++ js/detail.js
@@ -2,20 +2,43 @@ let getCard = {
   name: "Laptop Pro X",
   price: "$1200",
   description: "16GB RAM, 512GB SSD",
-  image: ["https://images.pexels.com/photos/18105/pexels-photo.jpg","https://images.pexels.com/photos/18105/pexels-photo.jpg","https://images.pexels.com/photos/18105/pexels-photo.jpg"],
+  image: [
+    "https://images.pexels.com/photos/18105/pexels-photo.jpg",
+    "https://images.pexels.com/photos/18105/pexels-photo.jpg",
+    "https://images.pexels.com/photos/18105/pexels-photo.jpg",
+  ],
+  supplier: "56598797",
+  ram: "8",
+  brand: "HP",
+  processor: "INTEL CORE I5",
+  hard_disk: "1000",
+  graphic_card: "Intel Iris Xe",
+  design: "NON",
+  screen_size: "15,6 pouces",
+  operating_system: "WINDOWS",
+  rating:"4.6",
 };
 
-function displayProduct(){
-    document.getElementById("product-title").textContent = getCard.name;
-    document.getElementById("product-price").textContent = getCard.price;
-    document.getElementById("product-description").textContent = getCard.description;
-    document.getElementById("mainImage").src = getCard.image[0];
+function displayProduct() {
+  document.getElementById("product-title").textContent = getCard.name;
+  document.getElementById("product-price").textContent = getCard.price;
+  document.getElementById("product-description").textContent = getCard.description;
+  document.getElementById("mainImage").src = getCard.image[0];
+  document.getElementById("supplier").textContent = getCard.supplier;
+  document.getElementById("RAM").textContent = getCard.ram;
+  document.getElementById("brand").textContent = getCard.brand;
+  document.getElementById("processor").textContent = getCard.processor;
+  document.getElementById("hard-disk").textContent = getCard.hard_disk;
+  document.getElementById("graphic-card").textContent = getCard.graphic_card;
+  document.getElementById("design").textContent = getCard.design;
+  document.getElementById("screen-size").textContent = getCard.screen_size;
+  document.getElementById("operating-system").textContent = getCard.operating_system;
+  document.getElementById("rating").textContent = getCard.rating;
 
-    for (let i = 1; i < getCard.image.length; i++) {
-        document.getElementById("img1").src = getCard.image[i];
-        document.getElementById("img2").src = getCard.image[i];
-        document.getElementById("img3").src = getCard.image[i];
-    }
+  for (let i = 1; i < getCard.image.length; i++) {
+    document.getElementById("img1").src = getCard.image[i];
+    document.getElementById("img2").src = getCard.image[i];
+  }
 }
 
 window.onload = displayProduct;
